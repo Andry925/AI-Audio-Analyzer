da1c1feb3987b490c8e95788ed2f5bc4200da462
Andry925 <andry925@ukr.net>
2024-10-22 20:40:28 +0300

    tests: add based tests for application

--- audio_analyzer/accounts/tests.py
+++ audio_analyzer/accounts/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your tests here.
+
+class TestAuthentication(APITestCase):
+
+    def setUp(self):
+        self.data = {
+            "username": "someone11",
+            "password": "Superpass@2004",
+            "email": "some@gmail.com",
+        }
+        UserCustomModel = get_user_model()
+        self.user = UserCustomModel.objects.create_user(**self.data)
+
+    def test_register_new_user(self):
+        data = {
+            "username": "someone21",
+            "email": "some2@gmail.com",
+            "password": "Superpass@2004"
+        }
+        response = self.client.post(
+            reverse('register-user'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_register_existing_user(self):
+        response = self.client.post(
+            reverse('register-user'),
+            data=self.data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_register_with_short_password(self):
+        data = {
+            "username": "someone31",
+            "email": "some3@gmail.com",
+            "password": "superrrrr"
+        }
+        response = self.client.post(
+            reverse('register-user'),
+            data=data,
+            format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_login(self):
+        response = self.client.post(
+            reverse('login-user'), data=self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_with_invalid_credentials(self):
+        data = {
+            "username": "someone11",
+            "password": "superpass2232",
+            "email": "some@gmail.com",
+        }
+        response = self.client.post(reverse('login-user'), data=data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_logout(self):
+        response = self.client.post(
+            reverse('login-user'), data=self.data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.token = RefreshToken.for_user(self.user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f'Bearer {self.token.access_token}')
+        response = self.client.post(reverse('logout-user'), format='json')
+        self.assertEqual(response.status_code, status.HTTP_205_RESET_CONTENT)
