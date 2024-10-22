cb113387c6262953942bf6c4c002eed5a5542f63
Andry925 <andry925@ukr.net>
2024-10-22 16:29:13 +0300

    feat: add login url

--- audio_analyzer/accounts/urls.py
+++ audio_analyzer/accounts/urls.py
@@ -4,7 +4,14 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from . import views
 
 urlpatterns = [
-    path('token-refresh', TokenRefreshView.as_view(), name='token-refresh'),
-    path('registration', views.UserRegistrationView.as_view(), name='register-user')
-
-]
+    path(
+        'token-refresh',
+        TokenRefreshView.as_view(),
+        name='token-refresh'),
+    path(
+        'registration',
+        views.UserRegistrationView.as_view(),
+        name='register-user'),
+    path('login',
+         views.UserLoginView.as_view(),
+         name='login-user')]
