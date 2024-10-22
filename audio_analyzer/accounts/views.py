26bc8bb0fdfe977009ec46a2a5957be0cf42cb71
Andry925 <andry925@ukr.net>
2024-10-22 15:55:38 +0300

    feat: implement user registration endpoints

--- audio_analyzer/accounts/views.py
+++ audio_analyzer/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserRegistrationView(APIView):
+    permission_classes = [permissions.AllowAny, ]
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if not (serializer.is_valid(raise_exception=True) and not request.user.is_authenticated):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user = serializer.create(request.data)
+        if user:
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data="User can not be found", status=status.HTTP_400_BAD_REQUEST)
