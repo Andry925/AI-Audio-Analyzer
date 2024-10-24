f2387c15b478f07dacc59b559bf003a388481efb
Andry925 <andry925@ukr.net>
2024-10-24 11:49:58 +0300

    fix: change media root inside settings.py

--- audio_analyzer/audio_analyzer/settings.py
+++ audio_analyzer/audio_analyzer/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'mediafiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 INTERNAL_IPS = [
     # ...
