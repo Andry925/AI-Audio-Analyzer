9df780f8a2d15da74b2e4b45dbce0974f5b38d41
Andry925 <andry925@ukr.net>
2024-10-25 13:57:59 +0300

    feat: add urls for tasks endpoints

--- audio_analyzer/audio_analyzer/urls.py
+++ audio_analyzer/audio_analyzer/urls.py
@@ -23,9 +23,15 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
+    path('api/v1/analyzer-task/', include('analyzer_task.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
+    urlpatterns = [
+        path(
+            '__debug__/',
+            include(
+                debug_toolbar.urls)),
+    ] + urlpatterns
