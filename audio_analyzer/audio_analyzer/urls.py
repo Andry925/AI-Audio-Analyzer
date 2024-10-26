b5a89843a732c14900da699222e2a8503c8df2b9
Andry925 <andry925@ukr.net>
2024-10-26 11:19:36 +0300

    documentation: add swagger documentation

--- audio_analyzer/audio_analyzer/urls.py
+++ audio_analyzer/audio_analyzer/urls.py
@@ -19,11 +19,28 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Stock Price Tracker",
+        default_version='1.0.0',
+        description="API documentation for Audio Analyzer",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/accounts/', include('accounts.urls')),
     path('api/v1/analyzer-task/', include('analyzer_task.urls')),
+    path(
+          'swagger/schema/',
+          schema_view.with_ui(
+              'swagger',
+              cache_timeout=0),
+          name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
