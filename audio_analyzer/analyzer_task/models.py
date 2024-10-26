5dc92cdd13bc8334e10ab912e3ff5839c84a09ba
Andry925 <andry925@ukr.net>
2024-10-26 11:51:49 +0300

    fix: add language validation

--- audio_analyzer/analyzer_task/models.py
+++ audio_analyzer/analyzer_task/models.py
@@ -14,20 +14,32 @@ DEFAULT_MIDJOURNEY_FORMULA = (
 def upload_to(instance, filename):
     return f'audios/{filename}'
 
-
 class AnalyzerTask(models.Model):
-    name = models.CharField(max_length=255)
-    audio_file_url = models.FileField(
-        upload_to=upload_to, null=True, blank=True)
-    audio_file_language = models.CharField(
-        max_length=255, null=True, blank=True)
+    AUDIO_LANGUAGE_CHOICES = [
+        ('uk', 'uk'),
+        ('eng', 'eng'),
+        ('rus', 'rus'),
+    ]
+
+    name = models.TextField(max_length=255)
+    audio_file_url = models.FileField(upload_to=upload_to, null=True, blank=True) # You can add more languages but by default there are three
+    audio_file_language = models.TextField(
+        max_length=20,
+        choices=AUDIO_LANGUAGE_CHOICES,
+        null=True,
+        blank=True
+    )
     task_text = models.TextField(blank=True)
     user_id = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name='tasks')
+        related_name='tasks'
+    )
     helper_instruction = models.TextField(
-        null=True, blank=True, default=DEFAULT_MIDJOURNEY_FORMULA)
+        null=True,
+        blank=True,
+        default=DEFAULT_MIDJOURNEY_FORMULA
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
